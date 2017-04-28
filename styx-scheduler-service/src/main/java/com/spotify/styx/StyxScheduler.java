52a0709b8767792e640c684888c3b4578a3350ef
Daniel Norberg <dano@spotify.com>
2017-04-28 17:42:46 +0900

    oops

--- styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
+++ styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
@@ -159,8 +159,7 @@ public class StyxScheduler implements AppInit {
 
     private Time time = Instant::now;
     private StorageFactory storageFactory = storage(StyxScheduler::storage);
-    private DockerRunnerFactory dockerRunnerFactory = (id, environment, stateManager1, scheduler1, stats) -> createDockerRunner(
-        id, environment, stateManager1, scheduler1, stats);
+    private DockerRunnerFactory dockerRunnerFactory = StyxScheduler::createDockerRunner;
     private ScheduleSources scheduleSources = () -> ServiceLoader.load(ScheduleSourceFactory.class);
     private StatsFactory statsFactory = StyxScheduler::stats;
     private ExecutorFactory executorFactory = Executors::newScheduledThreadPool;
