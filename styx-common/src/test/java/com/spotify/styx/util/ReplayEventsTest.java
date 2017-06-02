22d351b25e7ac88e9ff13e338255ccabb2af9300
Daniel Norberg <dano@spotify.com>
2017-06-02 15:30:01 +0900

    add test

--- styx-common/src/test/java/com/spotify/styx/util/ReplayEventsTest.java
+++ styx-common/src/test/java/com/spotify/styx/util/ReplayEventsTest.java
@@ -21,6 +21,7 @@
 package com.spotify.styx.util;
 
 import static com.github.npathai.hamcrestopt.OptionalMatchers.isPresent;
+import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.common.collect.Sets.newTreeSet;
 import static com.spotify.styx.state.RunState.State.DONE;
 import static com.spotify.styx.state.RunState.State.RUNNING;
@@ -35,15 +36,20 @@ import com.google.common.collect.ImmutableMap;
 import com.spotify.styx.model.Event;
 import com.spotify.styx.model.SequenceEvent;
 import com.spotify.styx.state.RunState;
+import com.spotify.styx.state.RunState.State;
 import com.spotify.styx.state.Trigger;
 import com.spotify.styx.storage.Storage;
-import com.spotify.styx.testdata.TestData;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Optional;
 import java.util.SortedSet;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class ReplayEventsTest {
   Storage storage;
 
@@ -114,4 +120,36 @@ public class ReplayEventsTest {
 
     assertThat(restoredRunState, is(Optional.empty()));
   }
+
+  @Test
+  @Parameters({
+      "3, SUBMITTED, true",
+      "3, SUBMITTED, false",
+      "4, RUNNING, true",
+      "4, RUNNING, false",
+  })
+  public void restoreRunStateForActiveInstance(long counter, State expectedState, boolean printLogs) throws Exception {
+    SortedSet<SequenceEvent> events = newTreeSet(SequenceEvent.COUNTER_COMPARATOR);
+    events.add(SequenceEvent.create(Event.triggerExecution(WORKFLOW_INSTANCE, Trigger.natural()),        0L, 0L));
+    events.add(SequenceEvent.create(Event.dequeue(WORKFLOW_INSTANCE),                                    1L, 1L));
+    events.add(SequenceEvent.create(Event.submit(WORKFLOW_INSTANCE, EXECUTION_DESCRIPTION, "exec-1"),    2L, 2L));
+    events.add(SequenceEvent.create(Event.submitted(WORKFLOW_INSTANCE, "exec-1"),                        3L, 3L));
+    events.add(SequenceEvent.create(Event.started(WORKFLOW_INSTANCE),                                    4L, 4L));
+
+    when(storage.readEvents(WORKFLOW_INSTANCE)).thenReturn(events);
+
+    Map<RunState, Long> runStates = ReplayEvents.replayActiveStates(
+        ImmutableMap.of(WORKFLOW_INSTANCE, counter), storage, printLogs);
+
+    assertThat(runStates.size(), is(1));
+
+    RunState restoredRunState = getOnlyElement(runStates.keySet());
+    long restoredCounter = getOnlyElement(runStates.values());
+
+    assertThat(restoredCounter, is(counter));
+    assertThat(restoredRunState.state(), is(expectedState));
+    assertThat(restoredRunState.data().trigger(), isPresent());
+    assertThat(restoredRunState.data().trigger().get(), is(Trigger.natural()));
+  }
+
 }
