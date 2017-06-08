a7ff5383cd5c6a902711095e1687b034fd510ecb
Fabrizio Demaria <fdema@spotify.com>
2017-06-08 19:34:27 +0200

    Update deprecated WorkflowInstanceExecutionDataTest

--- styx-common/src/test/java/com/spotify/styx/model/data/deprecated/WorkflowInstanceExecutionDataTest.java
+++ styx-common/src/test/java/com/spotify/styx/model/data/deprecated/WorkflowInstanceExecutionDataTest.java
@@ -24,11 +24,11 @@ import static com.spotify.styx.serialization.Json.OBJECT_MAPPER;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import com.spotify.styx.model.WorkflowInstance;
 import com.spotify.styx.model.data.ExecStatus;
 import com.spotify.styx.model.data.Execution;
 import com.spotify.styx.model.data.Trigger;
-import com.spotify.styx.model.data.WorkflowInstanceExecutionData;
+import com.spotify.styx.model.deprecated.WorkflowId;
+import com.spotify.styx.model.deprecated.WorkflowInstance;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Optional;
@@ -38,78 +38,9 @@ import org.junit.Test;
 public class WorkflowInstanceExecutionDataTest {
 
   @Test
-  public void shouldDeserializeExecStatus() throws Exception {
-    String json = statusJson("09:56", "STARTED");
-    ExecStatus executionStatus = OBJECT_MAPPER.readValue(json, ExecStatus.class);
-
-    assertThat(executionStatus.timestamp(), is(Instant.parse("2016-08-03T09:56:03.607Z")));
-    assertThat(executionStatus.status(), is("STARTED"));
-  }
-
-  @Test
-  public void shouldDeserializeExecution() throws Exception {
-    String json = executionJson("exec-id", "busybox:1.0", "09", "SUCCESS");
-
-    Execution execution = OBJECT_MAPPER.readValue(json, Execution.class);
-    Execution expected = Execution.create(
-        Optional.of("exec-id"),
-        Optional.of("busybox:1.0"),
-        Arrays.asList(
-            ExecStatus.create(Instant.parse("2016-08-03T09:56:03.607Z"), "STARTED", Optional.empty()),
-            ExecStatus.create(Instant.parse("2016-08-03T09:57:03.607Z"), "RUNNING", Optional.empty()),
-            ExecStatus.create(Instant.parse("2016-08-03T09:58:03.607Z"), "SUCCESS", Optional.empty())
-        )
-    );
-    assertThat(execution, is(expected));
-  }
-
-  @Test
-  public void shouldDeserializeTrigger() throws Exception {
-    String jsonExec0 = executionJson("exec-id-0", "busybox:1.0", "09", "FAILED");
-    String jsonExec1 = executionJson("exec-id-1", "busybox:1.1", "10", "SUCCESS");
-
-    String json =
-        "{"
-        + "\"trigger_id\":\"trig-0\","
-        + "\"timestamp\":\"" + time("07:55") + "\","
-        + "\"complete\":true,"
-        + "\"executions\":["
-        + jsonExec0 + "," + jsonExec1
-        + "]}";
-
-    Trigger trigger = OBJECT_MAPPER.readValue(json, Trigger.class);
-    Trigger expected = Trigger.create(
-        "trig-0",
-        time("07:55"),
-        true,
-        Arrays.asList(
-            Execution.create(
-                Optional.of("exec-id-0"),
-                Optional.of("busybox:1.0"),
-                Arrays.asList(
-                    ExecStatus.create(Instant.parse("2016-08-03T09:56:03.607Z"), "STARTED", Optional.empty()),
-                    ExecStatus.create(Instant.parse("2016-08-03T09:57:03.607Z"), "RUNNING", Optional.empty()),
-                    ExecStatus.create(Instant.parse("2016-08-03T09:58:03.607Z"), "FAILED", Optional.empty())
-                )
-            ),
-            Execution.create(
-                Optional.of("exec-id-1"),
-                Optional.of("busybox:1.1"),
-                Arrays.asList(
-                    ExecStatus.create(Instant.parse("2016-08-03T10:56:03.607Z"), "STARTED", Optional.empty()),
-                    ExecStatus.create(Instant.parse("2016-08-03T10:57:03.607Z"), "RUNNING", Optional.empty()),
-                    ExecStatus.create(Instant.parse("2016-08-03T10:58:03.607Z"), "SUCCESS", Optional.empty())
-                )
-            )
-        )
-    );
-    assertThat(trigger, is(expected));
-  }
-
-  @Test
   public void shouldDeserializeExecutionData() throws Exception {
-    String jsonExec00 = executionJson("exec-id-00", "busybox:1.0", "07", "FAILED");
-    String jsonExec01 = executionJson("exec-id-01", "busybox:1.1", "08", "SUCCESS");
+    String jsonExec00 = executionJson("exec-id-00", "busybox:1.0", "07", "FAILED", Optional.of("Exit code 1"));
+    String jsonExec01 = executionJson("exec-id-01", "busybox:1.1", "08", "SUCCESS", Optional.empty());
 
     String jsonTrigger0 =
         "{"
@@ -120,8 +51,8 @@ public class WorkflowInstanceExecutionDataTest {
         + jsonExec00 + "," + jsonExec01
         + "]}";
 
-    String jsonExec10 = executionJson("exec-id-10", "busybox:1.2", "09", "FAILED");
-    String jsonExec11 = executionJson("exec-id-11", "busybox:1.3", "10", "SUCCESS");
+    String jsonExec10 = executionJson("exec-id-10", "busybox:1.2", "09", "FAILED", Optional.of("Exit code 1"));
+    String jsonExec11 = executionJson("exec-id-11", "busybox:1.3", "10", "SUCCESS", Optional.empty());
 
     String jsonTrigger1 =
         "{"
@@ -137,7 +68,7 @@ public class WorkflowInstanceExecutionDataTest {
         + "\"workflow_instance\":{"
           + "\"workflow_id\":{"
             + "\"component_id\":\"component1\","
-            + "\"id\":\"endpoint1\""
+            + "\"endpoint_id\":\"endpoint1\""
           + "},"
           + "\"parameter\":\"2016-08-03T06\""
         + "},"
@@ -145,11 +76,11 @@ public class WorkflowInstanceExecutionDataTest {
         + jsonTrigger0 + "," + jsonTrigger1
         + "]}";
 
-    com.spotify.styx.model.data.WorkflowInstanceExecutionData
+    WorkflowInstanceExecutionData
         executionData = OBJECT_MAPPER.readValue(json, WorkflowInstanceExecutionData.class);
-    com.spotify.styx.model.data.WorkflowInstanceExecutionData
+    WorkflowInstanceExecutionData
         expected = WorkflowInstanceExecutionData.create(
-        WorkflowInstance.parseKey("component1#endpoint1#2016-08-03T06"),
+        WorkflowInstance.create(WorkflowId.create("component1", "endpoint1"), "2016-08-03T06"),
         Arrays.asList(
             Trigger.create(
                 "trig-0",
@@ -162,7 +93,7 @@ public class WorkflowInstanceExecutionDataTest {
                         Arrays.asList(
                             ExecStatus.create(Instant.parse("2016-08-03T07:56:03.607Z"), "STARTED", Optional.empty()),
                             ExecStatus.create(Instant.parse("2016-08-03T07:57:03.607Z"), "RUNNING", Optional.empty()),
-                            ExecStatus.create(Instant.parse("2016-08-03T07:58:03.607Z"), "FAILED", Optional.empty())
+                            ExecStatus.create(Instant.parse("2016-08-03T07:58:03.607Z"), "FAILED", Optional.of("Exit code 1"))
                         )
                     ),
                     Execution.create(
@@ -187,7 +118,7 @@ public class WorkflowInstanceExecutionDataTest {
                         Arrays.asList(
                             ExecStatus.create(Instant.parse("2016-08-03T09:56:03.607Z"), "STARTED", Optional.empty()),
                             ExecStatus.create(Instant.parse("2016-08-03T09:57:03.607Z"), "RUNNING", Optional.empty()),
-                            ExecStatus.create(Instant.parse("2016-08-03T09:58:03.607Z"), "FAILED", Optional.empty())
+                            ExecStatus.create(Instant.parse("2016-08-03T09:58:03.607Z"), "FAILED", Optional.of("Exit code 1"))
                         )
                     ),
                     Execution.create(
@@ -207,14 +138,15 @@ public class WorkflowInstanceExecutionDataTest {
     assertThat(executionData, is(expected));
   }
 
-  private String statusJson(String time, String status) {
-    return "{\"timestamp\":\"2016-08-03T" + time + ":03.607Z\", \"status\":\"" + status + "\"}";
+  private String statusJson(String time, String status, Optional<String> message) {
+    final String baseJson = "{\"timestamp\":\"2016-08-03T" + time + ":03.607Z\", \"status\":\"" + status + "\"";
+    return message.map(s -> baseJson + ", \"message\":\"" + s + "\"}").orElseGet(() -> baseJson + "}");
   }
 
-  private String executionJson(String id, String image, String hour, String endStatus) {
-    String jsonStatus1 = statusJson(hour + ":56", "STARTED");
-    String jsonStatus2 = statusJson(hour + ":57", "RUNNING");
-    String jsonStatus3 = statusJson(hour + ":58", endStatus);
+  private String executionJson(String id, String image, String hour, String endStatus, Optional<String> endMessage) {
+    String jsonStatus1 = statusJson(hour + ":56", "STARTED", Optional.empty());
+    String jsonStatus2 = statusJson(hour + ":57", "RUNNING", Optional.empty());
+    String jsonStatus3 = statusJson(hour + ":58", endStatus, endMessage);
 
     return
         "{"
