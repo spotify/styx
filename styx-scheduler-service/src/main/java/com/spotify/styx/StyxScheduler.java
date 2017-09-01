296628a2e14d96615b45836d5e4df9dcd1d1f824
Hongxin Liang <honnix@spotify.com>
2017-09-01 10:15:22 +0200

    register active states while starting up

--- styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
+++ styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
@@ -541,6 +541,10 @@ public class StyxScheduler implements AppInit {
               .count());
     });
 
+    workflowCache.all().forEach(workflow -> stats.registerActiveStatesMetric(
+        workflow.id(),
+        () -> stateManager.getActiveStatesCount(workflow.id())));
+
     stats.registerSubmissionRateLimitMetric(submissionRateLimiter::getRate);
   }
 
@@ -594,6 +598,7 @@ public class StyxScheduler implements AppInit {
       cache.remove(workflow);
     });
   }
+
   private static Stats stats(Environment environment) {
     return new MetricsStats(environment.resolve(SemanticMetricRegistry.class));
   }
