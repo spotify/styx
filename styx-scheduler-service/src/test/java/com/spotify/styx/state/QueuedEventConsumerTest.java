2016979901bb6e615cdc60e0b246cd9d79557627
Fabrizio Demaria <fdema@spotify.com>
2017-10-18 11:40:30 +0200

    Add test for QueuedEventConsumerTest

--- styx-scheduler-service/src/test/java/com/spotify/styx/state/QueuedEventConsumerTest.java
+++ styx-scheduler-service/src/test/java/com/spotify/styx/state/QueuedEventConsumerTest.java
@@ -25,33 +25,47 @@ import static org.awaitility.Awaitility.await;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import com.google.common.collect.Lists;
 import com.spotify.styx.model.Event;
 import com.spotify.styx.model.SequenceEvent;
 import com.spotify.styx.model.WorkflowId;
 import com.spotify.styx.model.WorkflowInstance;
 import com.spotify.styx.publisher.EventInterceptor;
+import java.util.List;
 import org.junit.Test;
 
 public class QueuedEventConsumerTest {
-  private SequenceEvent trackedEvent;
+  private List<SequenceEvent> trackedEvents = Lists.newArrayList();
   private QueuedEventConsumer eventConsumer = new QueuedEventConsumer(new InjectingInterceptor());
   private final static WorkflowInstance wfi = WorkflowInstance.create(
       WorkflowId.create("comp1", "work1"),
       "2017-01-01");
-  private final static SequenceEvent event = SequenceEvent.create(
+  private final static SequenceEvent firstEvent = SequenceEvent.create(
       Event.triggerExecution(wfi, Trigger.natural()), 0, 0);
+  private final static SequenceEvent secondEvent = SequenceEvent.create(
+      Event.dequeue(wfi), 0, 0);
 
   @Test
   public void shouldConsumeEvent() throws Exception {
-    eventConsumer.processedEvent(event);
-    await().atMost(5, SECONDS).until(() -> trackedEvent != null);
-    assertThat(trackedEvent, is(event));
+    eventConsumer.processedEvent(firstEvent);
+    await().atMost(5, SECONDS).until(() -> trackedEvents.get(0) != null);
+    assertThat(trackedEvents.get(0), is(firstEvent));
+  }
+
+  @Test
+  public void shouldConsumeMoreEvents() throws Exception {
+    eventConsumer.processedEvent(firstEvent);
+    eventConsumer.processedEvent(secondEvent);
+    await().atMost(5, SECONDS).until(() -> trackedEvents.get(0) != null);
+    await().atMost(5, SECONDS).until(() -> trackedEvents.get(1) != null);
+    assertThat(trackedEvents.get(0), is(firstEvent));
+    assertThat(trackedEvents.get(1), is(secondEvent));
   }
 
   @Test(expected = QueuedEventConsumer.IsClosed.class)
   public void shouldRejectEventIfClosed() throws Exception {
     eventConsumer.close();
-    eventConsumer.processedEvent(event);
+    eventConsumer.processedEvent(firstEvent);
   }
 
   @Test
@@ -59,16 +73,15 @@ public class QueuedEventConsumerTest {
     QueuedEventConsumer eventConsumer =
         new QueuedEventConsumer(new SlowInjectingInterceptor());
 
-    eventConsumer.processedEvent(event);
+    eventConsumer.processedEvent(firstEvent);
     eventConsumer.close();
-
-    assertThat(trackedEvent, is(event));
+    assertThat(trackedEvents.get(0), is(firstEvent));
   }
 
   private class InjectingInterceptor implements EventInterceptor {
     @Override
     public void interceptedEvent(SequenceEvent sequenceEvent) {
-      trackedEvent = sequenceEvent;
+      trackedEvents.add(sequenceEvent);
     }
   }
 
@@ -81,7 +94,7 @@ public class QueuedEventConsumerTest {
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
-      trackedEvent = sequenceEvent;
+      trackedEvents.add(sequenceEvent);
     }
   }
 }
