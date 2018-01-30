ed8a6212b8cbfb6f239dccec8d9b8df5de60b93b
Daniel Norberg <dano@spotify.com>
2018-01-30 16:50:31 +0900

    code style

--- styx-common/src/main/java/com/spotify/styx/storage/DatastoreStorage.java
+++ styx-common/src/main/java/com/spotify/styx/storage/DatastoreStorage.java
@@ -398,7 +398,8 @@ class DatastoreStorage {
     storeWithRetries(() -> datastore.put(activeStateToEntity(datastore.newKeyFactory(), workflowInstance, state)));
   }
 
-  static Entity activeStateToEntity(KeyFactory keyFactory, WorkflowInstance workflowInstance, PersistentWorkflowInstanceState state)
+  static Entity activeStateToEntity(KeyFactory keyFactory, WorkflowInstance workflowInstance,
+      PersistentWorkflowInstanceState state)
       throws JsonProcessingException {
     final Key key = activeWorkflowInstanceKey(keyFactory, workflowInstance);
     final Entity.Builder entity = Entity.newBuilder(key)
