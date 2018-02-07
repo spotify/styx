81ab341f4e55844db3db2cd923ab61b874bcbf39
Fabrizio Demaria <fdema@spotify.com>
2018-02-07 11:50:03 +0100

    Restore registerActiveStatesMetric

--- styx-scheduler-service/src/main/java/com/spotify/styx/monitoring/NoopStats.java
+++ styx-scheduler-service/src/main/java/com/spotify/styx/monitoring/NoopStats.java
@@ -39,6 +39,11 @@ final class NoopStats implements Stats {
   }
 
   @Override
+  public void registerActiveStatesMetric(WorkflowId workflowId, Gauge<Long> activeStatesCount) {
+    // nop
+  }
+
+  @Override
   public void registerWorkflowCountMetric(String status, Gauge<Long> workflowCount) {
     // nop
   }
