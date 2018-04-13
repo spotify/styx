cd5d872c0d1da94213e615aadfa1e57958609e4c
Daniel Norberg <dano@spotify.com>
2018-04-13 14:51:41 +0900

    move index bootstrap out of ctor

--- styx-common/src/main/java/com/spotify/styx/storage/InMemStorage.java
+++ styx-common/src/main/java/com/spotify/styx/storage/InMemStorage.java
@@ -305,6 +305,10 @@ public class InMemStorage implements Storage {
   }
 
   @Override
+  public void indexActiveWorkflowInstances() {
+  }
+
+  @Override
   public SortedSet<SequenceEvent> readEvents(WorkflowInstance workflowInstance) {
     final SortedSet<SequenceEvent> events = Sets.newTreeSet(SequenceEvent.COUNTER_COMPARATOR);
     writtenEvents.stream()
