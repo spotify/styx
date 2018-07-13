7ce301643621401cb9b21b731de932de05e8330b
Daniel Norberg <dano@spotify.com>
2018-07-13 12:21:46 +0900

    propagate resource counter update failure
    Also do not silently swallow TimeoutException in tests.

--- styx-scheduler-service/src/test/java/com/spotify/styx/state/QueuedStateManagerTest.java
+++ styx-scheduler-service/src/test/java/com/spotify/styx/state/QueuedStateManagerTest.java
@@ -22,6 +22,8 @@ package com.spotify.styx.state;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.stream.Collectors.toSet;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
@@ -79,6 +81,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.BiConsumer;
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -738,7 +741,7 @@ public class QueuedStateManagerTest {
     try {
       spied.receive(dequeueEvent).toCompletableFuture().get(1, MINUTES);
       fail();
-    } catch (Exception e) {
+    } catch (ExecutionException e) {
       // expected exception
     }
 
@@ -765,7 +768,7 @@ public class QueuedStateManagerTest {
     try {
       spied.receive(dequeueEvent).toCompletableFuture().get(1, MINUTES);
       fail();
-    } catch (Exception e) {
+    } catch (ExecutionException e) {
       // expected exception
     }
 
@@ -775,7 +778,8 @@ public class QueuedStateManagerTest {
   @Test
   public void shouldFailToUpdateResourceCountersOnDequeueDueToConflict() throws Exception {
     givenState(INSTANCE, State.QUEUED);
-    doThrow(new RuntimeException())
+    final RuntimeException rootCause = new RuntimeException("conflict!");
+    doThrow(rootCause)
         .when(transaction).updateCounter(shardedCounter, "resource1", 1);
 
     final Set<Resource> resources = ImmutableSet.of(Resource.create("resource1", 1));
@@ -788,8 +792,11 @@ public class QueuedStateManagerTest {
     try {
       spied.receive(dequeueEvent).toCompletableFuture().get(1, MINUTES);
       fail();
-    } catch (Exception e) {
-      // expected exception
+    } catch (ExecutionException e) {
+      final Throwable cause = e.getCause();
+      assertThat(cause, instanceOf(RuntimeException.class));
+      assertThat(cause.getMessage(), is("Failed to update resource counter for workflow instance: " + INSTANCE));
+      assertThat(cause.getSuppressed(), is(arrayContaining(rootCause)));
     }
 
     verify(spied, never()).receiveIgnoreClosed(eq(infoEvent), anyLong());
