efeeabc53b8dab16ede53f096bb945d8c8eee85f
Joakim Bergman <jocke@spotify.com>
2018-09-13 16:07:09 +0200

    add tests for failing calls to FutureOkHttpClient

--- styx-client/src/test/java/com/spotify/styx/client/FutureOkHttpClientTest.java
+++ styx-client/src/test/java/com/spotify/styx/client/FutureOkHttpClientTest.java
@@ -23,11 +23,13 @@ package com.spotify.styx.client;
 import static com.spotify.styx.client.OkHttpTestUtil.bytesOfRequestBody;
 import static com.spotify.styx.client.OkHttpTestUtil.responseBuilder;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
@@ -39,7 +41,9 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okio.ByteString;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
@@ -52,6 +56,9 @@ public class FutureOkHttpClientTest {
   public Call call;
   @Mock
   public OkHttpClient okHttpClient;
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
 
   private static final HttpUrl URI = Objects.requireNonNull(HttpUrl.parse("http://example.org/"));
 
@@ -79,6 +86,30 @@ public class FutureOkHttpClientTest {
 
     assertThat(future.isDone(), is(true));
     assertThat(future.join(), is(response));
+  }
+
+  @Test
+  public void testUnserializablePayload() {
+    class EmptyClass {}
+    thrown.expectCause(instanceOf(JsonProcessingException.class));
+    FutureOkHttpClient.forUri(URI, "POST", new EmptyClass());
+  }
+
+  @Test
+  public void testSomethingGoesWrong() throws IOException {
+    final FutureOkHttpClient client = FutureOkHttpClient.create(okHttpClient);
+    when(okHttpClient.newCall(any())).thenReturn(call);
+    final ArgumentCaptor<Callback> argumentCaptor = ArgumentCaptor.forClass(Callback.class);
+    doNothing().when(call).enqueue(argumentCaptor.capture());
+    final CompletableFuture<Response> future = client.send(REQUEST).toCompletableFuture();
+
+    final IOException exception = new IOException("oh noes!");
+    argumentCaptor.getValue().onFailure(call, exception);
+
+    assertThat(future.isDone(), is(true));
+
+    thrown.expectCause(is(exception));
+    future.join();
   }
 
   @Test
