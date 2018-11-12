dde5589dc52567ab7cc0d9bfea88b7848f9a368b
Daniel Norberg <dano@spotify.com>
2018-11-12 16:14:57 +0900

    disable http/2 & connection pooling
    Work around for https://github.com/square/okhttp/issues/3955

--- styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
+++ styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
@@ -30,6 +30,7 @@ import static com.spotify.styx.util.GuardedRunnable.runGuarded;
 import static com.spotify.styx.util.ShardedCounter.NUM_SHARDS;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
@@ -104,6 +105,7 @@ import eu.javaspecialists.tjsn.concurrency.stripedexecutor.StripedExecutorServic
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.NamespacedKubernetesClient;
+import io.fabric8.kubernetes.client.utils.HttpClientUtils;
 import io.opencensus.common.Scope;
 import io.opencensus.trace.Tracer;
 import io.opencensus.trace.Tracing;
@@ -113,6 +115,7 @@ import java.security.GeneralSecurityException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -127,6 +130,9 @@ import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
 import org.apache.hadoop.hbase.client.Connection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -761,7 +767,12 @@ public class StyxScheduler implements AppInit {
           .withNamespace(config.getString(GKE_CLUSTER_NAMESPACE))
           .build();
 
-      return clientFactory.apply(kubeConfig);
+      final OkHttpClient httpClient = HttpClientUtils.createHttpClient(kubeConfig).newBuilder()
+          .connectionPool(new ConnectionPool(0, 0, SECONDS))
+          .protocols(Collections.singletonList(Protocol.HTTP_1_1))
+          .build();
+
+      return clientFactory.apply(httpClient, kubeConfig);
     } catch (IOException e) {
       throw Throwables.propagate(e);
     }
@@ -773,5 +784,5 @@ public class StyxScheduler implements AppInit {
   }
 
   interface KubernetesClientFactory
-      extends Function<io.fabric8.kubernetes.client.Config, NamespacedKubernetesClient> { }
+      extends BiFunction<OkHttpClient, io.fabric8.kubernetes.client.Config, NamespacedKubernetesClient> { }
 }
--- styx-scheduler-service/src/test/java/com/spotify/styx/StyxSchedulerTest.java
+++ styx-scheduler-service/src/test/java/com/spotify/styx/StyxSchedulerTest.java
@@ -62,9 +62,12 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import io.fabric8.kubernetes.client.NamespacedKubernetesClient;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.time.Instant;
 import java.util.Map;
 import java.util.function.Supplier;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -77,6 +80,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class StyxSchedulerTest {
 
   @Captor private ArgumentCaptor<io.fabric8.kubernetes.client.Config> k8sClientConfigCaptor;
+  @Captor private ArgumentCaptor<OkHttpClient> httpClientCaptor;
   @Captor private ArgumentCaptor<Shard> shardArgumentCaptor;
   @Captor private ArgumentCaptor<Gauge<Long>> longGaugeCaptor;
   @Captor private ArgumentCaptor<Gauge<Double>> doubleGaugeCaptor;
@@ -97,7 +101,7 @@ public class StyxSchedulerTest {
 
   @Before
   public void setUp() throws Exception {
-    when(kubernetesClientFactory.apply(any())).thenReturn(kubernetesClient);
+    when(kubernetesClientFactory.apply(any(), any())).thenReturn(kubernetesClient);
     when(gkeClient.projects().locations().clusters().get(any())).thenReturn(gkeClusterGet);
 
     styxScheduler = StyxScheduler.newBuilder().build();
@@ -137,15 +141,24 @@ public class StyxSchedulerTest {
         StyxScheduler.getKubernetesClient(config, "foo", gkeClient, kubernetesClientFactory);
     assertThat(client, is(theInstance(kubernetesClient)));
 
-    verify(kubernetesClientFactory).apply(k8sClientConfigCaptor.capture());
+    verify(kubernetesClientFactory).apply(httpClientCaptor.capture(), k8sClientConfigCaptor.capture());
 
     final io.fabric8.kubernetes.client.Config k8sConfig = k8sClientConfigCaptor.getValue();
+    final OkHttpClient httpClient = httpClientCaptor.getValue();
 
     assertThat(k8sConfig.getMasterUrl(), is("https://" + endpoint + "/"));
     assertThat(k8sConfig.getCaCertData(), is(clusterCaCertificate));
     assertThat(k8sConfig.getClientCertData(), is(clientCertificate));
     assertThat(k8sConfig.getClientKeyData(), is(clientKey));
     assertThat(k8sConfig.getNamespace(), is(namespace));
+
+    final Field maxIdleConnections = ConnectionPool.class.getDeclaredField("maxIdleConnections");
+    maxIdleConnections.setAccessible(true);
+    final Field keepAliveDurationNs = ConnectionPool.class.getDeclaredField("keepAliveDurationNs");
+    keepAliveDurationNs.setAccessible(true);
+
+    assertThat(keepAliveDurationNs.get(httpClient.connectionPool()), is(0L));
+    assertThat(maxIdleConnections.get(httpClient.connectionPool()), is(0));
   }
 
   @Test
