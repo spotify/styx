e4ccafb3a143ba4f521b2a29721d79f0f13dbd90
Daniel Norberg <dano@spotify.com>
2018-11-12 16:14:57 +0900

    disable http/2
    Work around for https://github.com/square/okhttp/issues/3955

--- styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
+++ styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
@@ -104,6 +104,7 @@ import eu.javaspecialists.tjsn.concurrency.stripedexecutor.StripedExecutorServic
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.NamespacedKubernetesClient;
+import io.fabric8.kubernetes.client.utils.HttpClientUtils;
 import io.opencensus.common.Scope;
 import io.opencensus.trace.Tracer;
 import io.opencensus.trace.Tracing;
@@ -113,6 +114,7 @@ import java.security.GeneralSecurityException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -127,6 +129,8 @@ import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
 import org.apache.hadoop.hbase.client.Connection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -761,7 +765,11 @@ public class StyxScheduler implements AppInit {
           .withNamespace(config.getString(GKE_CLUSTER_NAMESPACE))
           .build();
 
-      return clientFactory.apply(kubeConfig);
+      final OkHttpClient httpClient = HttpClientUtils.createHttpClient(kubeConfig).newBuilder()
+          .protocols(Collections.singletonList(Protocol.HTTP_1_1))
+          .build();
+
+      return clientFactory.apply(httpClient, kubeConfig);
     } catch (IOException e) {
       throw Throwables.propagate(e);
     }
@@ -773,5 +781,5 @@ public class StyxScheduler implements AppInit {
   }
 
   interface KubernetesClientFactory
-      extends Function<io.fabric8.kubernetes.client.Config, NamespacedKubernetesClient> { }
+      extends BiFunction<OkHttpClient, io.fabric8.kubernetes.client.Config, NamespacedKubernetesClient> { }
 }
