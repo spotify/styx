50201ec1afbe26cee24039296be1e2ffaa9e77c5
Nelson Arapé <nelson@spotify.com>
2019-01-21 09:36:05 +0100

    Rework test to make it parametrized
    ... and please Codacy's static analysis

--- styx-common/src/test/java/com/spotify/styx/util/MaybeCachedValueTest.java
+++ styx-common/src/test/java/com/spotify/styx/util/MaybeCachedValueTest.java
@@ -23,20 +23,35 @@ package com.spotify.styx.util;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class MaybeCachedValueTest {
+
+  @Parameterized.Parameters(name = "{index}: create({0}, {1})")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        { false, 0 }, { true, 1 }, { false, 2 }, { true, 3 }
+    });
+  }
+
+  private boolean cached;
+  private int value;
+
+  public MaybeCachedValueTest(boolean cached, int value) {
+    this.cached = cached;
+    this.value = value;
+  }
 
   @Test
   public void createShouldAssignProperties() {
-    testFor(true, 1);
-    testFor(false, 2);
-  }
+    MaybeCachedValue<Integer> maybeCacheValue = MaybeCachedValue.create(cached, value);
 
-  public void testFor(boolean expectedCache, int expectedValue) {
-    MaybeCachedValue<Integer> maybeCacheValue = MaybeCachedValue.create(expectedCache, expectedValue);
-
-    assertThat(maybeCacheValue.isCached(), equalTo(expectedCache));
-    assertThat(maybeCacheValue.value(), equalTo(expectedValue));
+    assertThat(maybeCacheValue.isCached(), equalTo(cached));
+    assertThat(maybeCacheValue.value(), equalTo(value));
   }
 }
