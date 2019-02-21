0d9dd038330931a08ba12da999e353774bf67f04
Daniel Norberg <dano@spotify.com>
2019-02-21 11:35:33 +0900

    fix test

--- styx-scheduler-service/src/test/java/com/spotify/styx/state/handlers/TerminationHandlerTest.java
+++ styx-scheduler-service/src/test/java/com/spotify/styx/state/handlers/TerminationHandlerTest.java
@@ -23,15 +23,15 @@ package com.spotify.styx.state.handlers;
 import static com.spotify.styx.state.RunState.State.FAILED;
 import static com.spotify.styx.state.RunState.State.TERMINATED;
 import static com.spotify.styx.state.handlers.TerminationHandler.MAX_RETRY_COST;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.spotify.styx.model.Event;
 import com.spotify.styx.model.WorkflowInstance;
 import com.spotify.styx.state.RunState;
 import com.spotify.styx.state.StateData;
-import com.spotify.styx.state.StateManager;
 import com.spotify.styx.testdata.TestData;
 import com.spotify.styx.util.RetryUtil;
 import java.time.Duration;
@@ -45,7 +45,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class TerminationHandlerTest {
 
-  @Mock StateManager stateManager;
   @Mock RetryUtil retryUtil;
 
   private TerminationHandler terminationHandler;
@@ -62,8 +61,8 @@ public class TerminationHandlerTest {
   public void shouldCompleteOnZeroExitCode() throws Exception {
     StateData data = data(1, 1.0, Optional.of(0));
     RunState zeroTerm = RunState.create(WORKFLOW_INSTANCE, TERMINATED, data);
-    terminationHandler.transitionInto(zeroTerm);
-    verify(stateManager).receiveIgnoreClosed(Event.success(WORKFLOW_INSTANCE));
+    var event = terminationHandler.transitionInto(zeroTerm).orElseThrow();
+    assertThat(event, is(Event.success(WORKFLOW_INSTANCE)));
   }
 
   @Test
@@ -72,8 +71,8 @@ public class TerminationHandlerTest {
     RunState nonZeroTerm = RunState.create(WORKFLOW_INSTANCE, TERMINATED, data);
     Duration expectedDelay = Duration.ofMillis(4711);
     when(retryUtil.calculateDelay(anyInt())).thenReturn(expectedDelay);
-    terminationHandler.transitionInto(nonZeroTerm);
-    verify(stateManager).receiveIgnoreClosed(Event.retryAfter(WORKFLOW_INSTANCE, expectedDelay.toMillis()));
+    var event = terminationHandler.transitionInto(nonZeroTerm).orElseThrow();
+    assertThat(event, is(Event.retryAfter(WORKFLOW_INSTANCE, expectedDelay.toMillis())));
   }
 
   @Test
@@ -82,8 +81,8 @@ public class TerminationHandlerTest {
     RunState nonZeroTerm = RunState.create(WORKFLOW_INSTANCE, TERMINATED, data);
     Duration expectedDelay = Duration.ofMillis(4711);
     when(retryUtil.calculateDelay(anyInt())).thenReturn(expectedDelay);
-    terminationHandler.transitionInto(nonZeroTerm);
-    verify(stateManager).receiveIgnoreClosed(Event.retryAfter(WORKFLOW_INSTANCE, expectedDelay.toMillis()));
+    var event = terminationHandler.transitionInto(nonZeroTerm).orElseThrow();
+    assertThat(event, is(Event.retryAfter(WORKFLOW_INSTANCE, expectedDelay.toMillis())));
   }
 
   @Test
@@ -92,40 +91,40 @@ public class TerminationHandlerTest {
     RunState failed = RunState.create(WORKFLOW_INSTANCE, FAILED, data);
     Duration expectedDelay = Duration.ofMillis(4711);
     when(retryUtil.calculateDelay(anyInt())).thenReturn(expectedDelay);
-    terminationHandler.transitionInto(failed);
-    verify(stateManager).receiveIgnoreClosed(Event.retryAfter(WORKFLOW_INSTANCE, expectedDelay.toMillis()));
+    var event = terminationHandler.transitionInto(failed).orElseThrow();
+    assertThat(event, is(Event.retryAfter(WORKFLOW_INSTANCE, expectedDelay.toMillis())));
   }
 
   @Test
   public void shouldStopOnNonZeroMaxRetriesReached() throws Exception {
     StateData data = data(400, MAX_RETRY_COST, Optional.of(1));
     RunState maxedTerm = RunState.create(WORKFLOW_INSTANCE, TERMINATED, data);
-    terminationHandler.transitionInto(maxedTerm);
-    verify(stateManager).receiveIgnoreClosed(Event.stop(WORKFLOW_INSTANCE));
+    var event = terminationHandler.transitionInto(maxedTerm).orElseThrow();
+    assertThat(event, is(Event.stop(WORKFLOW_INSTANCE)));
   }
 
   @Test
   public void shouldStopOnFailMaxRetriesReached() throws Exception {
     StateData data = data(400, MAX_RETRY_COST, Optional.of(1));
     RunState maxedTerm = RunState.create(WORKFLOW_INSTANCE, FAILED, data);
-    terminationHandler.transitionInto(maxedTerm);
-    verify(stateManager).receiveIgnoreClosed(Event.stop(WORKFLOW_INSTANCE));
+    var event = terminationHandler.transitionInto(maxedTerm).orElseThrow();
+    assertThat(event, is(Event.stop(WORKFLOW_INSTANCE)));
   }
 
   @Test
   public void shouldScheduleRetryOf10MinutesOnMissingDependencies() throws Exception {
     StateData data = data(1, 1.0, Optional.of(20));
     RunState missingDeps = RunState.create(WORKFLOW_INSTANCE, TERMINATED, data);
-    terminationHandler.transitionInto(missingDeps);
-    verify(stateManager).receiveIgnoreClosed(Event.retryAfter(WORKFLOW_INSTANCE, Duration.ofMinutes(10).toMillis()));
+    var event = terminationHandler.transitionInto(missingDeps).orElseThrow();
+    assertThat(event, is(Event.retryAfter(WORKFLOW_INSTANCE, Duration.ofMinutes(10).toMillis())));
   }
 
   @Test
   public void shouldFailOnFailFastExitCodeReceived() throws Exception {
     StateData data = data(1, 1.0, Optional.of(50));
     RunState maxedTerm = RunState.create(WORKFLOW_INSTANCE, FAILED, data);
-    terminationHandler.transitionInto(maxedTerm);
-    verify(stateManager).receiveIgnoreClosed(Event.stop(WORKFLOW_INSTANCE));
+    var event = terminationHandler.transitionInto(maxedTerm).orElseThrow();
+    assertThat(event, is(Event.stop(WORKFLOW_INSTANCE)));
   }
 
   private StateData data(int tries, double cost, Optional<Integer> lastExit) {
