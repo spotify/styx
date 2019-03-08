b86b42a16edf2783608eada1e2f4fcce89ad378d
Daniel Norberg <dano@spotify.com>
2019-03-08 16:58:13 +0900

    please codacy

--- styx-common/src/test/java/com/spotify/styx/storage/DatastoreStorageTest.java
+++ styx-common/src/test/java/com/spotify/styx/storage/DatastoreStorageTest.java
@@ -45,6 +45,7 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
@@ -439,7 +440,7 @@ public class DatastoreStorageTest {
   public void readActiveStatesShouldNotPropagateIOException() throws Exception {
     final IOException cause = new IOException("foobar");
     doThrow(cause).when(datastore).query(any());
-    storage.readActiveStates();
+    assertThat(storage.readActiveStates(), is(notNullValue()));
   }
 
   @Test
