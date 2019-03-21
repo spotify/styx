dfc12889f691fddbf5683dafb3c9cb2d5a46676e
Nelson Arapé <nelson@spotify.com>
2019-03-21 11:34:25 +0100

    Remove unused imports
    to please static analysis checker

--- styx-scheduler-service/src/main/java/com/spotify/styx/state/StateManager.java
+++ styx-scheduler-service/src/main/java/com/spotify/styx/state/StateManager.java
@@ -28,8 +28,6 @@ import java.io.Closeable;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.CompletionStage;
-import java.util.function.Predicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
