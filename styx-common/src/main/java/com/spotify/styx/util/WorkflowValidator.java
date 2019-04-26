8b3a68f904dce929283b5d255d17fd03264c7846
tnsetting <38207208+tnsetting@users.noreply.github.com>
2019-04-26 16:11:26 +0200

    Update WorkflowValidator.java

--- styx-common/src/main/java/com/spotify/styx/util/WorkflowValidator.java
+++ styx-common/src/main/java/com/spotify/styx/util/WorkflowValidator.java
@@ -31,7 +31,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WorkflowValidator {
