df8b49d86b371ffae0193f758ce47dc23eed471f
Daniel Norberg <dano@spotify.com>
2019-05-30 10:19:46 +0900

    annotate testlogger as thread safe

--- styx-test/src/main/java/com/spotify/styx/TestLogger.java
+++ styx-test/src/main/java/com/spotify/styx/TestLogger.java
@@ -26,6 +26,7 @@ import org.junit.runner.notification.RunListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@RunListener.ThreadSafe
 public class TestLogger extends RunListener {
 
   private final Logger log;
