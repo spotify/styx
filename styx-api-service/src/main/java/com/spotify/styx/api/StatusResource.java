d0269e2cde49048e08ce8bfedd94411d27c83942
niant <niant@spotify.com>
2019-09-27 14:21:44 +0200

    Revert "Use a new thread pood for active states"
    This reverts commit 8fdcdc24c7877e71ed88ed2eb74aefc27117c85b.

--- styx-api-service/src/main/java/com/spotify/styx/api/StatusResource.java
+++ styx-api-service/src/main/java/com/spotify/styx/api/StatusResource.java
@@ -21,12 +21,11 @@
 package com.spotify.styx.api;
 
 import static com.spotify.styx.api.Api.Version.V3;
-import static com.spotify.styx.util.CloserUtil.register;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toList;
 
 import com.google.api.client.util.Lists;
-import com.google.common.io.Closer;
+import com.google.common.collect.ImmutableMap;
 import com.spotify.apollo.RequestContext;
 import com.spotify.apollo.Response;
 import com.spotify.apollo.Status;
@@ -50,9 +49,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
 import java.util.stream.Stream;
 import okio.ByteString;
 import org.slf4j.Logger;
@@ -64,19 +60,15 @@ import org.slf4j.LoggerFactory;
 public class StatusResource {
 
   private static final Logger log = LoggerFactory.getLogger(StatusResource.class);
-  private static final int CONCURRENCY = 32;
 
   static final String BASE = "/status";
 
   private final Storage storage;
   private final ServiceAccountUsageAuthorizer accountUsageAuthorizer;
-  private final ForkJoinPool forkJoinPool;
-  private final Closer closer = Closer.create();
 
   public StatusResource(Storage storage, ServiceAccountUsageAuthorizer accountUsageAuthorizer) {
     this.storage = requireNonNull(storage);
     this.accountUsageAuthorizer = requireNonNull(accountUsageAuthorizer);
-    this.forkJoinPool = register(closer, new ForkJoinPool(CONCURRENCY), "status-resource");
   }
 
   public Stream<Route<AsyncHandler<Response<ByteString>>>> routes() {
@@ -154,36 +146,25 @@ public class StatusResource {
 
   private Map<WorkflowInstance, RunState> getActiveStates(String componentsStr) throws IOException {
     final List<String> components = Arrays.asList(componentsStr.split(","));
-    final ConcurrentHashMap<WorkflowInstance, RunState> concurrentHashMap = new ConcurrentHashMap<>();
+    final ImmutableMap.Builder<WorkflowInstance, RunState> mapBuilder = ImmutableMap.builder();
 
-    final List<Optional<IOException>> exceptions =
-        components.stream()
-            .map(
-                componentId ->
-                    forkJoinPool.submit(
-                        () -> {
-                          Optional<IOException> exception = Optional.empty();
-                          try {
-                            final Map<WorkflowInstance, RunState> stateMap =
-                                storage.readActiveStates(componentId);
-                            for (WorkflowInstance instance : stateMap.keySet()) {
-                              concurrentHashMap.put(instance, stateMap.get(instance));
-                            }
-                          } catch (IOException e) {
-                            exception = Optional.of(e);
-                          }
-                          return exception;
-                        }))
-            .collect(toList())
-            .stream()
-            .map(ForkJoinTask::join)
-            .filter(Optional::isPresent)
-            .collect(toList());
+    final List<Optional<IOException>> exceptions = components.parallelStream().map( componentId -> {
+      Optional<IOException> exception = Optional.empty();
+      try{
+        final Map<WorkflowInstance, RunState> stateMap = storage.readActiveStates(componentId);
+        for (WorkflowInstance instance : stateMap.keySet()) {
+          mapBuilder.put(instance, stateMap.get(instance));
+        }
+      } catch (IOException e) {
+        exception = Optional.of(e);
+      }
+      return exception;
+      }).filter(Optional::isPresent).collect(toList());
     if (!exceptions.isEmpty()) {
       throw exceptions.get(0).orElseThrow();
     }
 
-    return concurrentHashMap;
+    return mapBuilder.build();
   }
 
   private Map<WorkflowInstance, RunState> getActiveStates(Optional<String> componentOpt, Optional<String> workflowOpt)
