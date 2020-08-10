ec0101da2dfd1f4c4e2c84ca04bcb5ea8b62d959
Babis Kiosidis <ckiosidis@gmail.com>
2020-08-10 15:41:31 +0300

    reapply license headers

--- styx-flyte-client/src/main/java/com/spotify/styx/flyte/client/LaunchPlanIdentifier.java
+++ styx-flyte-client/src/main/java/com/spotify/styx/flyte/client/LaunchPlanIdentifier.java
@@ -1,8 +1,8 @@
 /*-
  * -\-\-
- * Spotify Styx Testing Utilities
+ * Spotify Styx Flyte Client
  * --
- * Copyright (C) 2016 - 2019 Spotify AB
+ * Copyright (C) 2016 - 2020 Spotify AB
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
