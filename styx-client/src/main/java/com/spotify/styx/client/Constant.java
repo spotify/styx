34392859c61d3b15c71171c29ff4dedc04cccc5b
Hongxin Liang <honnix@spotify.com>
2020-09-20 22:21:10 +0200

    Fix license header

--- styx-client/src/main/java/com/spotify/styx/client/Constant.java
+++ styx-client/src/main/java/com/spotify/styx/client/Constant.java
@@ -2,7 +2,7 @@
  * -\-\-
  * Spotify Styx API Client
  * --
- * Copyright (C) 2016 - 2018 Spotify AB
+ * Copyright (C) 2020 Spotify AB
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
