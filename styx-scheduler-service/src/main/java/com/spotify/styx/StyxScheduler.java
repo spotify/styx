e7d5fcb8e83c6a6fed74a54d4d0209ad7d8d434e
Sonja Ericsson <sonjae@spotify.com>
2020-11-25 17:22:42 +0100

    Change order of closing things

--- styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
+++ styx-scheduler-service/src/main/java/com/spotify/styx/StyxScheduler.java
@@ -364,9 +364,7 @@ public class StyxScheduler implements AppInit {
     final Publisher publisher = publisherFactory.apply(environment);
     closer.register(publisher);
 
-    // TODO: is the shutdown timeout of 1 second here sane?
-    final ScheduledExecutorService tickExecutor = executorFactory.create(3, tickTf);
-    closer.register(closeable(tickExecutor, "tick-executor", Duration.ofSeconds(1)));
+
     var stateProcessingExecutor = Executors.newWorkStealingPool(
         optionalInt(config, STYX_STATE_PROCESSING_THREADS).orElse(DEFAULT_STYX_STATE_PROCESSING_THREADS));
     closer.register(closeable(stateProcessingExecutor, "state-processing", Duration.ofSeconds(1)));
@@ -459,6 +457,10 @@ public class StyxScheduler implements AppInit {
 
     final Duration stateManagerTickInterval = get(config, config::getDuration, STYX_STATE_MANAGER_TICK_INTERVAL)
         .orElse(DEFAULT_STATE_MANAGER_TICK_INTERVAL);
+
+    // TODO: is the shutdown timeout of 1 second here sane?
+    final ScheduledExecutorService tickExecutor = executorFactory.create(3, tickTf);
+    closer.register(closeable(tickExecutor, "tick-executor", Duration.ofSeconds(1)));
 
     startTriggerManager(triggerManager, tickExecutor, triggerTickInterval);
     startBackfillTriggerManager(backfillTriggerManager, tickExecutor, triggerTickInterval);
