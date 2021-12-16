c0749a316eef41953504ecce42e790b49b7cc50b
Babis Kiosidis <ckiosidis@gmail.com>
2021-12-16 10:29:49 +0200

    fix shouldComplain test

--- styx-flyte-client/src/main/java/com/spotify/styx/flyte/client/FlyteInputsUtils.java
+++ styx-flyte-client/src/main/java/com/spotify/styx/flyte/client/FlyteInputsUtils.java
@@ -115,8 +115,8 @@ public class FlyteInputsUtils {
     var paramsKeysInLowercase = parameterMap.getParametersMap().keySet().stream().map(String::toLowerCase).collect(toSet());
     var lowercaseInputs = extraDefaultInputs.keySet().stream().map(String::toLowerCase).collect(toSet());
     if (!paramsKeysInLowercase.containsAll(lowercaseInputs)) {
-      var unMatchedInputKeys = parameterMap.getParametersMap().keySet().stream()
-          .filter(key -> !paramsKeysInLowercase.contains(key.toLowerCase()))
+      var unMatchedInputKeys = lowercaseInputs.stream()
+          .filter(key -> !paramsKeysInLowercase.contains(key))
           .collect(toList());
       throw new UnsupportedOperationException("Inputs don't correspond with launch plans inputs:"
                                               + " " + unMatchedInputKeys);
