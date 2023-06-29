0b30611446f5e9d5bdde23769e29c15f2d1a2666
Honnix <honnix@users.noreply.github.com>
2023-06-29 09:37:31 +0200

    Lenient header matching (#1091)
    * Matching headers as substring

--- styx-service-common/src/test/java/com/spotify/styx/api/MiddlewaresTest.java
+++ styx-service-common/src/test/java/com/spotify/styx/api/MiddlewaresTest.java
@@ -538,7 +538,9 @@ public class MiddlewaresTest {
     RequestContext requestContext = mock(RequestContext.class);
     Request request = Request.forUri("/", "PUT")
         .withPayload(ByteString.encodeUtf8("hello"))
-        .withHeader(HttpHeaders.AUTHORIZATION, "Bearer s3cr3tp455w0rd");
+        .withHeader(HttpHeaders.AUTHORIZATION, "Bearer s3cr3tp455w0rd")
+        .withHeader("foo-service-identity", "Bearer s3cr3tp455w0rd")
+        .withHeader("foo-bar", "foo-bar");
     when(requestContext.request()).thenReturn(request);
 
     String email = "foo@bar.net";
@@ -556,7 +558,7 @@ public class MiddlewaresTest {
         request.method(),
         request.uri(),
         email,
-        Map.of(HttpHeaders.AUTHORIZATION, "<hidden>"),
+        Map.of(HttpHeaders.AUTHORIZATION, "<hidden>", "foo-service-identity", "<hidden>", "foo-bar", "foo-bar"),
         Map.of(),
         request.payload().orElseThrow().utf8());
   }
